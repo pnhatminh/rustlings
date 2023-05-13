c1246c29cfdf22d463d5cdcaf3143e36ff2a9e41
Minh Pham <minh.pham@codelink.io>
2023-05-13 12:17:17 +0700

    finish hashmap. need revision

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -13,8 +13,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -39,6 +37,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the basket.
         // Note that you are not allowed to put any type of fruit that's already
         // present!
+        match fruit {
+            Fruit::Banana => basket.insert(fruit, 10),
+            Fruit::Pineapple => basket.insert(fruit, 5),
+            (_) => continue
+        };
     }
 }
 
